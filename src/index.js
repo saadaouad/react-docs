d9f4bdb9c8648bee1594090673fcf671d5663401
saadaouad <aouadsaad05@gmail.com>
2017-08-05 02:17:19 +0100

    Improve structure app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import HellWorld from './hello-world/index.js';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<HellWorld />, document.getElementById('root'));
 registerServiceWorker();
