89757a01f49feb75c2564f85b6075b1e84acb095
saadaouad <aouadsaad05@gmail.com>
2018-01-28 13:52:14 +0000

    Make an assertion

--- cypress/integration/simple_spec.js
+++ cypress/integration/simple_spec.js
@@ -1,6 +1,10 @@
 describe('My First Test', function() {
     it('Clicks an element', function() {
       cy.visit('https://example.cypress.io/')
+
       cy.contains('blur').click()
+
+      cy.url()
+        .should('include', '/commands/actions')
     })
   })
