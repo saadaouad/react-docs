2c97c13f9ec9824454038ad53f926d0d6f704569
saadaouad <aouadsaad05@gmail.com>
2018-02-22 02:58:35 +0000

    test: Testing Our App
    Visit our server with write our own integration testing
    reconfigure cypress.json with adding a baseUrl and prefix cy.visit()

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -1,5 +1,5 @@
 describe('The Home Page', function() {
   it('successfully loads', function() {
-    cy.visit('http://localhost:3000'); // change URL to match your dev URL
+    cy.visit('/'); // URL added in cypress.json
   });
 });
