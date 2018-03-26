b675076ceee0dddae486c59f12b7901897f8cfda
saadaouad <aouadsaad05@gmail.com>
2018-03-26 02:16:12 +0100

    Don't run My First Test

--- cypress/integration/simple_spec.js
+++ cypress/integration/simple_spec.js
@@ -1,14 +1,14 @@
-describe('My First Test', function() {
-    it('Clicks an element', function() {
-      cy.visit('https://example.cypress.io/')
+// describe('My First Test', function() {
+//     it('Clicks an element', function() {
+//       cy.visit('https://example.cypress.io/')
 
-      cy.contains('blur').click()
+//       cy.contains('blur').click()
 
-      cy.url()
-        .should('include', '/commands/actions')
+//       cy.url()
+//         .should('include', '/commands/actions')
 
-      cy.get('.action-email')
-        .type('fake@email.com')
-        .should('have.value', 'fake@email.com')
-    })
-  })
+//       cy.get('.action-email')
+//         .type('fake@email.com')
+//         .should('have.value', 'fake@email.com')
+//     })
+//   })
