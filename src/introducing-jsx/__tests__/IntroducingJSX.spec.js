6fbd71a6d1bdac411a6758ecd49445c42981d603
saadouad <aouadsaad05@gmail.com>
2018-12-03 03:15:51 +0100

    Write introducing JSX test

--- src/introducing-jsx/__tests__/IntroducingJSX.spec.js
+++ src/introducing-jsx/__tests__/IntroducingJSX.spec.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import IntroducingJSX from '../index.js';
+import React from "react";
+import { shallow } from "enzyme";
+import IntroducingJSX from "../index.js";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<IntroducingJSX />, div);
+const user = {
+  firstName: "Mohammed",
+  lastName: "Abu muslim"
+};
+
+it("IntroducingJSX component renders without crashing", () => {
+  const wrapper = shallow(<IntroducingJSX user={user} />);
+  function formatName(user) {
+    return user.firstName + " " + user.lastName;
+  }
+  const formatedName = <p>Hello, {formatName(user)}!</p>;
+  expect(wrapper.contains(formatedName)).toEqual(true);
 });
