8e2ffb03e06e9b8f8f2afb50308426fb6d220ca7
saadouad <aouadsaad05@gmail.com>
2018-12-03 03:04:04 +0100

    Write components and props test

--- src/components-and-props/__tests__/Avatar.spec.js
+++ src/components-and-props/__tests__/Avatar.spec.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Avatar from '../Avatar.js';
+import React from "react";
+import { shallow } from "enzyme";
+import Avatar from "../Avatar.js";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Avatar />, div);
+it("Avatar component renders without crashing", () => {
+  const wrapper = shallow(<Avatar avatarUrl="https://s.yimg.com/ap/build/images/refencing-announcement/bird2.jpg" name="Canar" />);
+  const picture = (
+    <img className="Avatar" src="https://s.yimg.com/ap/build/images/refencing-announcement/bird2.jpg" alt="Canar" />
+  );
+  expect(wrapper.contains(picture)).toEqual(true);
 });
--- src/components-and-props/__tests__/UserInfo.spec.js
+++ src/components-and-props/__tests__/UserInfo.spec.js
@@ -1,8 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import UserInfo from '../UserInfo.js';
+import React from "react";
+import { shallow } from "enzyme";
+import UserInfo from "../UserInfo.js";
+import Avatar from "../Avatar.js";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<UserInfo />, div);
+it("UserInfo component renders without crashing", () => {
+  const wrapper = shallow(
+    <UserInfo
+      avatarUrl="https://s.yimg.com/ap/build/images/refencing-announcement/bird2.jpg"
+      name="Canar"
+    />
+  );
+  const name = <h2>This is, Canar!</h2>;
+  const picture = (
+    <Avatar
+      avatarUrl="https://s.yimg.com/ap/build/images/refencing-announcement/bird2.jpg"
+      name="Canar"
+    />
+  );
+  expect(wrapper.contains(name)).toEqual(true);
+  expect(wrapper.contains(picture)).toEqual(true);
 });
