c86d22e0f0e0c2bbb30971d84ee3147947c807ed
saadouad <aouadsaad05@gmail.com>
2019-01-21 02:16:51 +0100

    Add main component

---
+++ src/components/main-component/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class MainComponent extends React.Component {
+  render() {
+    return (
+      <div className="tc pv3">
+        <p className="f4">
+          This is an implementation of React official documentation{" "}
+          <a target="_blank" rel="noopener noreferrer" href="https://reactjs.org/docs/getting-started.html">
+            https://reactjs.org/docs/getting-started.html
+          </a>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default MainComponent;
