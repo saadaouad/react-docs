51fbc6ccc679473ae336157a1435bf613c70e6cc
saadouad <aouadsaad05@gmail.com>
2019-01-21 02:03:44 +0100

    Add link home to header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
+import { Link } from "@reach/router";
 
 class Header extends Component {
   render() {
     return (
       <div className="bg-light-blue tl pa3">
-        <h2>React docs!</h2>
+        <Link to="/" className="no-underline black-80">
+          <h2 className="dim">React docs!</h2>
+        </Link>
       </div>
     );
   }
