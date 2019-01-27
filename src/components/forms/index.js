69b925cda6c393873deaa02eb97f1693fa74842d
saadouad <aouadsaad05@gmail.com>
2019-01-27 23:21:33 +0100

    Write simple Formik form

--- src/components/forms/index.js
+++ src/components/forms/index.js
@@ -1,9 +1,71 @@
 import React from "react";
+import { Formik } from "formik";
 
-class Forms extends React.Component {
-  render() {
-    return <div className="tc">This is form</div>;
-  }
-}
+const Forms = () => (
+  <div className="tc">
+    <h2>Forms</h2>
+    <div className="tl pv3">
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        validate={values => {
+          let errors = {};
+          if (!values.email) {
+            errors.email = "Required";
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+          ) {
+            errors.email = "Invalid email address";
+          }
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting
+          /* and other goodies */
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <div className="pa2">
+              <input
+                type="email"
+                name="email"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.email}
+              />
+              {errors.email && touched.email && errors.email}
+            </div>
+            <div className="pa2">
+              <input
+                type="password"
+                name="password"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.password}
+              />
+              {errors.password && touched.password && errors.password}
+            </div>
+            <div className="pa2">
+              <button type="submit" disabled={isSubmitting}>
+                Submit
+              </button>
+            </div>
+          </form>
+        )}
+      </Formik>
+    </div>
+  </div>
+);
 
 export default Forms;
