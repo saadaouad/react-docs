5be5402cca8f27f4f0f5cb08bc6cef6a6d4ef130
saadouad <aouadsaad05@gmail.com>
2019-02-24 22:12:33 +0100

    Add func BoilingVerdict

--- src/components/lifting-state-up/index.js
+++ src/components/lifting-state-up/index.js
@@ -1,5 +1,12 @@
 import React from "react";
 
+function BoilingVerdict(props) {
+  if (props.celsius >= 100) {
+    return <p>The water would boil.</p>;
+  }
+  return <p>The water would not boil.</p>;
+}
+
 class LiftingStateUp extends React.Component {
   render() {
     return <div>Salam</div>;
