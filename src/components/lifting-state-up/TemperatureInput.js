09ea2783e22eb1195a03b4fd62db30dd0a6c8413
saadouad <aouadsaad05@gmail.com>
2019-03-09 20:47:48 +0100

    Add TemperatureInput and Calculator components

---
+++ src/components/lifting-state-up/TemperatureInput.js
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const scaleNames = {
+  c: "Celsius",
+  f: "Fahrenheit"
+};
+
+class TemperatureInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.props.onTemperatureChange(e.target.value);
+  }
+
+  render() {
+    const {temperature, scale} = this.props; 
+    return (
+      <fieldset>
+        <legend>Enter temperature in {scaleNames[scale]}:</legend>
+        <input value={temperature} onChange={this.handleChange} />
+      </fieldset>
+    );
+  }
+}
+
+TemperatureInput.propTypes = {
+  temperature: PropTypes.string.isRequired,
+  scale: PropTypes.string.isRequired,
+  onTemperatureChange: PropTypes.func.isRequired,
+}
+
+export default TemperatureInput;
