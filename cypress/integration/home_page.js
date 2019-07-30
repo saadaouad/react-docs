be626379f88a4ddff1befd0e38b5665f927f68d1
saadouad <aouadsaad05@gmail.com>
2019-07-30 15:30:24 +0100

    Write e2e tests for home page

--- cypress/integration/home_page.js
+++ cypress/integration/home_page.js
@@ -1,8 +1,22 @@
-describe("The home page", function() {
-  it("The home page", function() {
+describe("Home page", function() {
+  it("visit home page", function() {
     cy.visit("/");
   });
-  it("Home page redirection", function() {
-    cy.get("[data-cy=header-title]").click({force: true});
+
+  it("check home page redirection", function() {
+    cy.get("[data-cy=header-title]").click({ force: true });
+    cy.url().should("include", "/");
+  });
+
+  it("check main component content", function() {
+    cy.get("[data-cy=main-component]").contains(
+      "This is an implementation of all React official docs sections using CRA"
+    );
+    cy.get("[data-cy=react-docs").click();
+    cy.get("[data-cy=create-react-app]").click();
+  });
+
+  it("check footer content", function() {
+    cy.get("[data-cy=footer]").contains("Happy hacking!");
   });
 });
