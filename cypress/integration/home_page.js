ba1fdad830735fe4ef9cbefc6442f5bc6d9314bf
saadouad <aouadsaad05@gmail.com>
2019-07-30 03:39:57 +0100

    Fix ci

--- cypress/integration/home_page.js
+++ cypress/integration/home_page.js
@@ -1,6 +1,8 @@
 describe("The home page", function() {
   it("The home page", function() {
     cy.visit("/");
-    cy.get("[data-cy=header-title]").click();
+  });
+  it("Home page redirection", function() {
+    cy.get("[data-cy=header-title]").click({force: true});
   });
 });
