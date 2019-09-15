afdbe5c8ac018872749b6d42aeaac012c524362d
saadouad <aouadsaad05@gmail.com>
2019-09-15 03:45:32 +0100

    Write e2e tests for lists and keys component

--- src/components/lists-and-keys/index.js
+++ src/components/lists-and-keys/index.js
@@ -3,13 +3,13 @@ import React from "react";
 const ListItems = () => {
   const numbers = [1, 2, 3, 4, 5];
   const listItems = numbers.map((number, index) => (
-    <li key={index} className="list-items pv1">
+    <li data-cy="list-items" key={index} className="list-items pv1">
       {number}
     </li>
   ));
   return (
     <div className="tc">
-      <h2>Lists and keys</h2>
+      <h2 data-cy="title">Lists and keys</h2>
       <div className="tl pa3">{listItems}</div>
     </div>
   );
