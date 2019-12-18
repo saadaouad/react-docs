7d3e0fd2b31c6d8c9d070e91fafe34e5cf6b4725
saadouad <aouadsaad05@gmail.com>
2019-12-19 00:38:49 +0100

    Converts ES6 classes to functional components

--- src/components/code-splitting/AnotherLazyComponent.js
+++ src/components/code-splitting/AnotherLazyComponent.js
@@ -1,16 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-class AnotherLazyComponent extends Component {
-  render() {
-    const {dataCy} = this.props;
-    return (
-      <div className="tc">
-        <h4 data-cy={dataCy}>Another lazy component</h4>
-      </div>
-    );
-  }
-}
+const AnotherLazyComponent = props => {
+  const {dataCy} = props;
+  return (
+    <div className="tc">
+      <h4 data-cy={dataCy}>Another lazy component</h4>
+    </div>
+  );
+};
 
 AnotherLazyComponent.propTypes = {
   dataCy: PropTypes.string.isRequired,
--- src/components/code-splitting/LazyComponent.js
+++ src/components/code-splitting/LazyComponent.js
@@ -1,16 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-class LazyComponent extends Component {
-  render() {
-    const {dataCy} = this.props;
-    return (
-      <div className="tc">
-        <h4 data-cy={dataCy}>Lazy component</h4>
-      </div>
-    );
-  }
-}
+const LazyComponent = props => {
+  const {dataCy} = props;
+  return (
+    <div className="tc">
+      <h4 data-cy={dataCy}>Lazy component</h4>
+    </div>
+  );
+};
 
 LazyComponent.propTypes = {
   dataCy: PropTypes.string.isRequired,
