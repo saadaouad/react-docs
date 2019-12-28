6f09e16d34e731238fb7f4ef80da29db77b968e8
saadouad <aouadsaad05@gmail.com>
2019-12-28 15:26:27 +0100

    Integrate error boundaries

---
+++ src/components/error-boundary/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
+    console.log(error, {
+      info,
+      currenturl: typeof window !== "undefined" && window.location.href
+    });
+    // todo raise error on a tracking error software
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <p className="pa2">Something went wrong</p>;
+    }
+
+    return this.props.children;
+  }
+}
+
+ErrorBoundary.propTypes = {
+  children: PropTypes.node.isRequired
+};
+
+export default ErrorBoundary;
