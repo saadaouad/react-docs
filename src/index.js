7ad0e88a3d78c4b3ccf832c7bfd1889a093f56a2
saadouad <aouadsaad05@gmail.com>
2020-05-23 15:08:36 +0000

    Fix build

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Home from './routes/index.js';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import "react-app-polyfill/ie11"; // For IE 11 support
+import "react-app-polyfill/ie9"; // For IE 9-11 support
+import ReactDOM from "react-dom";
+import "./index.scss";
+import registerServiceWorker from "./registerServiceWorker";
+import Home from "./routes/index.js";
 
-import 'react-app-polyfill/ie9'; // For IE 9-11 support
-import 'react-app-polyfill/ie11'; // For IE 11 support
-
-ReactDOM.render(<Home />, document.getElementById('root'));
+ReactDOM.render(<Home />, document.getElementById("root"));
 registerServiceWorker();
