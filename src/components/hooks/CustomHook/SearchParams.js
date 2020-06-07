1869891a2be764744d2f2a58728748eaceecfe47
saadouad <aouadsaad05@gmail.com>
2020-06-07 13:51:32 +0100

    Persist state with Context Hooks

--- src/components/hooks/CustomHook/SearchParams.js
+++ src/components/hooks/CustomHook/SearchParams.js
@@ -1,5 +1,6 @@
 import pet, { ANIMALS } from "@frontendmasters/pet";
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import ThemeContext from "./ThemeContext";
 import useDropDown from "./useDropDown";
 
 const SearchParams = () => {
@@ -7,6 +8,7 @@ const SearchParams = () => {
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropDown("Animal", "dog", ANIMALS);
   const [, BreedDropdown, setBreed] = useDropDown("Breed", "", breeds);
+  const [theme, setTheme] = useContext(ThemeContext);
 
   useEffect(() => {
     setBreeds([]);
@@ -40,7 +42,25 @@ const SearchParams = () => {
         <div className="pv2">
           <BreedDropdown />
         </div>
-        <button>Submit</button>
+        <div className="pv2">
+          <label htmlFor="theme">
+            Theme
+            <select
+              value={theme}
+              onChange={e => setTheme(e.target.value)}
+              onBlur={e => setTheme(e.target.value)}
+              className="mh2"
+            >
+              <option value="peru">Peru</option>
+              <option value="yellow">Yellow</option>
+              <option value="mediumorchid">Medium Orchid</option>
+              <option value="chartreuse">Chartreuse</option>
+            </select>
+          </label>
+        </div>
+        <button id="submit" style={{ backgroundColor: theme }}>
+          Submit
+        </button>
       </form>
     </div>
   );
